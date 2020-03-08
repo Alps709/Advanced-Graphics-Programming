912078ce9b13c23cad6a046a63049ec984f5e9b7
Anton Le Prevost-Smith <alps709@hotmail.com>
2020-03-09 10:50:00 +1300

    Fixed a bug of it not accounting for the texture co-ords when rendering

--- OpenGL Graphics Programming/TerrainMesh.cpp
+++ OpenGL Graphics Programming/TerrainMesh.cpp
@@ -47,8 +47,8 @@ void TerrainMesh::SetVertexAttributes()
 	//3 normal floats
 	vbInfo.Push<float>(3);
 
-	//4 colour floats
-	//vbInfo.Push<float>(4);
+	//2 tex co-ord ints
+	vbInfo.Push<unsigned>(2);
 
 	AddVAOBuffer(vbInfo);
 }
@@ -72,61 +72,66 @@ void TerrainMesh::GenerateTerrainMesh(unsigned int _xSize, unsigned int _zSize)
 {
 	FastNoise noiseGenerator;
 	noiseGenerator.SetNoiseType(FastNoise::Perlin);
-
 	noiseGenerator.SetGradientPerturbAmp(50);
 	
 	const int SIZE = 800;
-	const int VERTEX_COUNT = 128;
-	const int count = VERTEX_COUNT * VERTEX_COUNT;
+	const int count = _xSize * _zSize;
 
-	std::vector<float> vertices(count * 6);
+	const int topLeftX = (_xSize - 1) / -2.0f;
+	const int topLeftZ = (_zSize - 1) / 2.0f;
 
-	std::vector<unsigned int> indices(6 * (VERTEX_COUNT - 1) * (VERTEX_COUNT - 1));
+	std::vector<float> vertices(count * 8);
+
+	std::vector<unsigned int> indices((_xSize - 1) * (_zSize - 1) * 6);
 
 	int vertexPointer = 0;
 
 	int tempX = 0;
 	int tempZ = 0;
-	for (int i = 0; i < 128; i++) 
+	for (int z = 0; z < _zSize; z++)
 	{
-		for (int j = 0; j < 128; j++) 
+		//Variable of current x position to give to the noise generator
+		tempZ = topLeftZ - z;
+
+		for (int x = 0; x < _xSize; x++)
 		{
-			//Variables of current x and z positions to give to the noise generator
-			tempX = (float)j / ((float)VERTEX_COUNT - 1) * SIZE;
-			tempZ = (float)i / ((float)VERTEX_COUNT - 1) * SIZE;
+			//Variable of current x position to give to the noise generator
+			tempX = topLeftX + x;
 
 			//Positions
-			vertices[vertexPointer * 3]     = tempX;
-			vertices[vertexPointer * 3 + 1] = noiseGenerator.GetNoise(tempX, tempZ) * 50;
-			vertices[vertexPointer * 3 + 2] = tempZ;
-
-			//Normals
-			vertices[vertexPointer * 3 + 3] = 0;
-			vertices[vertexPointer * 3 + 4] = 1;
-			vertices[vertexPointer * 3 + 5] = 0;
-
-			//Texture co-ords
-			vertices[vertexPointer * 3 + 6] = (float)j / ((float)VERTEX_COUNT - 1);
-			vertices[vertexPointer * 3 + 7] = (float)i / ((float)VERTEX_COUNT - 1);
-			vertexPointer++;
+			vertices[vertexPointer]     = tempX * SIZE;
+			vertices[vertexPointer + 1] = noiseGenerator.GetNoise(tempX, tempZ) * 50;
+			vertices[vertexPointer + 2] = tempZ * SIZE;
+									
+			//Normals				
+			vertices[vertexPointer + 3] = 0;
+			vertices[vertexPointer + 4] = 1;
+			vertices[vertexPointer + 5] = 0;
+									
+			//Texture co-ords		
+			vertices[vertexPointer + 6] = (float)x / ((float)_xSize - 1);
+			vertices[vertexPointer + 7] = (float)z / ((float)_zSize - 1);
+			vertexPointer += 8;
 		}
 	}
 
 	int pointer = 0;
-	for (int gz = 0; gz < VERTEX_COUNT - 1; gz++) 
+	for (int gz = 0; gz < _zSize - 1; gz++) 
 	{
-		for (int gx = 0; gx < VERTEX_COUNT - 1; gx++) 
+		for (int gx = 0; gx < _xSize - 1; gx++) 
 		{
-			int topLeft = (gz * VERTEX_COUNT) + gx;
-			int topRight = topLeft + 1;
-			int bottomLeft = ((gz + 1) * VERTEX_COUNT) + gx;
-			int bottomRight = bottomLeft + 1;
-			indices[pointer++] = topLeft;
-			indices[pointer++] = bottomLeft;
-			indices[pointer++] = topRight;
-			indices[pointer++] = topRight;
-			indices[pointer++] = bottomLeft;
-			indices[pointer++] = bottomRight;
+			if (gx < _zSize - 1 && gz < _xSize - 1)
+			{
+				indices[pointer]     = pointer;
+				indices[pointer + 1] = pointer + _xSize + 1;
+				indices[pointer + 2] = pointer + _zSize;
+
+				indices[pointer + 3] = pointer + _xSize + 1;
+				indices[pointer + 4] = pointer;
+				indices[pointer + 5] = pointer + 1;
+
+				pointer += 6;
+			}
 		}
 	}
 
