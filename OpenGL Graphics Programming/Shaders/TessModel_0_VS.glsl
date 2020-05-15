8b0b59b138c273a73b9bbdbcbe2ec1aefb963574
Anton Le Prevost-Smith <alps709@hotmail.com>
2020-05-16 04:38:45 +1200

    Reworked the tesselation shader to calculate everything in world space
    The vertex shader multiplies the local vertex position by the PVM matrix so that the tessellation shader doesn't have to multiply the PVM matrix for every generated position

--- OpenGL Graphics Programming/Shaders/TessModel_0_VS.glsl
+++ OpenGL Graphics Programming/Shaders/TessModel_0_VS.glsl
@@ -2,7 +2,9 @@
 
 layout (location = 0) in vec3 position;
 
+uniform mat4 u_PVM;
+
 void main(void)
 {
-	gl_Position = vec4(position, 1.0f);
+	gl_Position = u_PVM * vec4(position, 1.0f);
 }
--- OpenGL Graphics Programming/Shaders/TessModel_1_TCS.glsl
+++ OpenGL Graphics Programming/Shaders/TessModel_1_TCS.glsl
@@ -9,13 +9,13 @@ void main()
 {
 	if (gl_InvocationID == 0) 
 	{
-		gl_TessLevelInner[0] = 6.0 / u_cameraDistance;
-		gl_TessLevelInner[1] = 6.0 / u_cameraDistance;
+		gl_TessLevelInner[0] = 4.0 / u_cameraDistance;
+		gl_TessLevelInner[1] = 4.0 / u_cameraDistance;
 
-		gl_TessLevelOuter[0] = 10.0 / u_cameraDistance;
-		gl_TessLevelOuter[1] = 10.0 / u_cameraDistance;
-		gl_TessLevelOuter[2] = 10.0 / u_cameraDistance;
-		gl_TessLevelOuter[3] = 10.0 / u_cameraDistance;
+		gl_TessLevelOuter[0] = 6.0 / u_cameraDistance;
+		gl_TessLevelOuter[1] = 6.0 / u_cameraDistance;
+		gl_TessLevelOuter[2] = 6.0 / u_cameraDistance;
+		gl_TessLevelOuter[3] = 6.0 / u_cameraDistance;
 
 	}	
 
